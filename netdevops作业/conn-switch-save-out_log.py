2e234459089c8ed906b50b6745b777f91630036c
zhaoxd <1101930659@qq.com>
2021-06-08 21:03:22 +0800

    netmiko-v1

--- netdevops作业/conn-switch-save-out_log.py
+++ netdevops作业/conn-switch-save-out_log.py
@@ -3,7 +3,7 @@ from netmiko import ConnectHandler
 
 if __name__ == "__main__":
     net_conn = ConnectHandler(device_type = 'cisco_ios',
-                              host ='192.168.0.250',
+                              host ='192.168.199.200',
                               username ='admin',
                               password ='cisco',
                               port ='22',
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     output = net_conn.send_command('show ip interface  brief')
     net_conn.disconnect()
 
-    with open('isco_show_int_br.log', mode='w',encoding='utf8') as f:
+    with open('cisco_show_int_br.log', mode='w',encoding='utf8') as f:
         f.write(output)
