108b071fa3254551863eb107f92a0b3f2243b490
yxl-wsl <1101930659@qq.com>
2022-01-03 22:04:50 +0800

    change go  project

---
+++ netdevops作业/go/package-test/Test1/helloworld-1.go
@@ -0,0 +1,17 @@
+// helloworld.go
+package main
+
+import (
+"fmt"
+"myMath"
+)
+//单行注释
+/*
+注释第一行
+注释第二行
+*/
+func main(){
+    fmt.Println("Hello World!")
+    fmt.Println(mathClass.Add(1,1))
+ 	fmt.Println(mathClass.Sub(1,1))
+}
